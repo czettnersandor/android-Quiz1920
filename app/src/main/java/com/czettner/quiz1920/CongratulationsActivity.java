17ff3c824d412454c9962577737197283c309e05
Sándor Czettner <sandor@czettner.hu>
2016-07-18 21:55:34 +0100

    CongratulationsActivity

--- app/src/main/java/com/czettner/quiz1920/CongratulationsActivity.java
+++ app/src/main/java/com/czettner/quiz1920/CongratulationsActivity.java
@@ -2,16 +2,12 @@ package com.czettner.quiz1920;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class QuizActivity extends AppCompatActivity {
+public class CongratulationsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
-    }
-    public void onSubmit(View view) {
-
+        setContentView(R.layout.activity_congratulations);
     }
 }
